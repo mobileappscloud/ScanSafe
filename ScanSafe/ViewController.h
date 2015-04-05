d5975daf98ed5af3fe327016dcd2c2bb3c39dc6a
Faisal_Syed <doc92606@gmail.com>
2015-04-05 11:43:18 -0700

    Initial Commit

---
+++ ScanSafe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanSafe
+//
+//  Created by Mac on 4/5/15.
+//  Copyright (c) 2015 CongenialApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
